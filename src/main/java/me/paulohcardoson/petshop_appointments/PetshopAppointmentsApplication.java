39bf9dc34a11e572aabec0f025846948fc94da8d
Paulo Henrique Cardoso do Nascimento <pauloh.cardoson@gmail.com>
2026-01-13 22:31:50 -0400

    feat:Initializing app

---
+++ src/main/java/me/paulohcardoson/petshop_appointments/PetshopAppointmentsApplication.java
@@ -0,0 +1,13 @@
+package me.paulohcardoson.petshop_appointments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshopAppointmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetshopAppointmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/paulohcardoson/petshop_appointments/PetshopAppointmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.paulohcardoson.petshop_appointments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopAppointmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
